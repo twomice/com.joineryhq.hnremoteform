fd6b78b3e1408962c8dbc5b22f5f9b32d744bb9d
lucky091588 <lucky091588@gmail.com>
2020-12-13 03:50:59 +0800

    Replace ts() to E::ts()

--- CRM/Hnremoteform/Form/Settings.php
+++ CRM/Hnremoteform/Form/Settings.php
@@ -64,15 +64,15 @@ class CRM_Hnremoteform_Form_Settings extends CRM_Core_Form {
           default:
             $add = 'add' . $setting['quick_form_type'];
             if ($add == 'addElement') {
-              $this->$add($setting['html_type'], $name, ts($setting['title']), CRM_Utils_Array::value('html_attributes', $setting, array()));
+              $this->$add($setting['html_type'], $name, E::ts($setting['title']), CRM_Utils_Array::value('html_attributes', $setting, array()));
             }
             else {
-              $this->$add($name, ts($setting['title']));
+              $this->$add($name, E::ts($setting['title']));
             }
             break;
         }
       }
-      $descriptions[$setting['name']] = ts($setting['description']);
+      $descriptions[$setting['name']] = E::ts($setting['description']);
 
       if (!empty($setting['X_form_rules_args'])) {
         $rules_args = (array)$setting['X_form_rules_args'];
@@ -87,7 +87,7 @@ class CRM_Hnremoteform_Form_Settings extends CRM_Core_Form {
     $this->addButtons(array(
       array (
         'type' => 'submit',
-        'name' => ts('Submit'),
+        'name' => E::ts('Submit'),
         'isDefault' => TRUE,
       )
     ));
@@ -159,7 +159,7 @@ class CRM_Hnremoteform_Form_Settings extends CRM_Core_Form {
     $unsettings = array_fill_keys(array_keys(array_diff_key($settings, $this->_submittedValues)), NULL);
     _hnremoteform_civicrmapi('setting', 'create', $unsettings);
 
-    CRM_Core_Session::setStatus(" ", ts('Settings saved.'), "success");
+    CRM_Core_Session::setStatus(" ", E::ts('Settings saved.'), "success");
   }
 
   /**
@@ -176,7 +176,7 @@ class CRM_Hnremoteform_Form_Settings extends CRM_Core_Form {
 
   public static function getCustomFieldOptions() {
     $options = array();
-    $options = array(0 => '- '. ts('none') . ' -');
+    $options = array(0 => '- '. E::ts('none') . ' -');
     $result = _hnremoteform_civicrmapi('CustomGroup', 'get', [
       'extends' => "contribution",
       'api.CustomField.get' => [
